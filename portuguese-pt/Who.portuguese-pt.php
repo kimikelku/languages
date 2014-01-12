98b3fcd7d81b7520ff29625c29b44e018548888d
Ricardo <sismov7db@gmail.com>
2014-01-12 14:04:40 +0000

    2 more files added
    I'll probably come back to review them since i don't know if im going to
    use a term, the translation for portuguese seems to formal for the
    context.

---
+++ portuguese-pt/Who.portuguese-pt.php
@@ -0,0 +1,156 @@
+<?php
+// Version: 2.0; Who
+
+$txt['who_hidden'] = '<em>Nada, ou nada que possa ver...</em>';
+$txt['who_unknown'] = '<em>Unknown Action</em>';
+$txt['who_user'] = 'Utilizador';
+$txt['who_time'] = 'Tempo';
+$txt['who_action'] = 'Ação';
+$txt['who_show1'] = 'Mostrar ';
+$txt['who_show_members_only'] = 'Membros apenas';
+$txt['who_show_guests_only'] = 'Visitantes apenas';
+$txt['who_show_spiders_only'] = 'Spiders apenas';
+$txt['who_show_all'] = 'Todos';
+$txt['who_no_online_spiders'] = 'De momento não se encontra online nenhuma spider.';
+$txt['who_no_online_guests'] = 'De momento não se encontra online nenhum visitante.';
+$txt['who_no_online_members'] = 'De momento não se encontra online nenhum membro.';
+$txt['who_guest_login'] = 'Utilizador foi levado para a página de inicio de sessão.';
+
+$txt['whospider_login'] = 'A visualizar a página de inicio de sessão.';
+$txt['whospider_register'] = 'A visualizar a página de registo.';
+$txt['whospider_reminder'] = 'A visualizar a página de lembranças.';
+
+$txt['whoall_activate'] = 'A ativar a conta.';
+$txt['whoall_boards'] = 'A visualizar o índice do fórum <a href="<URL>">{forum_name}</a>.';
+$txt['whoall_buddy'] = 'A modificar a lista de contatos.';
+$txt['whoall_coppa'] = 'A preencher a ficha de controlo parental.';
+$txt['whoall_credits'] = 'A visualizar a página de créditos.';
+$txt['whoall_emailuser'] = 'A enviar um email a outro membro.';
+$txt['whoall_groups'] = 'A visualizar a página de grupos do membro.';
+$txt['whoall_help'] = 'A visualizar uma popup de ajuda.';
+$txt['whoall_pm'] = 'A visualizar as suas mensagens.';
+$txt['whoall_login'] = 'A autenticar-se no fórum.';
+
+$txt['whoall_login2'] = 'iniciando a sessão no fórum.';
+$txt['whoall_logout'] = 'Terminando a sessão no fórum.';
+$txt['whoall_markasread'] = 'Marcando os tópicos como lidos ou não lidos.';
+$txt['whoall_news'] = 'A visualizar as novidades.';
+$txt['whoall_notify'] = 'A modificar as definições de notificações.';
+$txt['whoall_notifyboard'] = 'A modificar as definições de notificações.';
+$txt['whoall_quickmod'] = 'A moderar um fórum.';
+$txt['whoall_recent'] = 'A visualizar uma <a href="<URL>?action=recent">lista de tópicos recentes</a>.';
+$txt['whoall_register'] = 'A registar uma conta no fórum.';
+$txt['whoall_register2'] = 'A registar uma conta no fórum.';
+$txt['whoall_reminder'] = 'A requisitar uma recuperação da palavra-passe.';
+$txt['whoall_report'] = 'A reportar um tópico a um moderador.';
+$txt['whoall_skin'] = 'A escolher uma skin diferente.';
+$txt['whoall_thoughts'] = 'A visualizar o arquivo de pensamentos.';
+$txt['whoall_unread'] = 'A visualizar tópicos por ler desde a sua última visita.';
+$txt['whoall_unreadreplies'] = 'A visualizar respostas por ler desde a sua última visita.';
+$txt['whoall_who'] = 'A visualizar <a href="<URL>?action=who">quem está Online</a>.';
+
+$txt['whoall_collapse_collapse'] = 'A contrair a categoria.';
+$txt['whoall_collapse_expand'] = 'A expandir a categoria.';
+$txt['whoall_pm_removeall'] = 'A remover todas as mensagens.';
+$txt['whoall_pm_send'] = 'A enviar a mensagem.';
+$txt['whoall_pm_send2'] = 'A enviar uma mensagem.';
+
+$txt['whotopic_announce'] = 'A anunciar o tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+$txt['whotopic_dlattach'] = 'A visualizar um anexo.';
+$txt['whotopic_deletemsg'] = 'A apagar uma mensagem.';
+$txt['whotopic_jsmodify'] = 'A modificar um post &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+$txt['whotopic_lock'] = 'A fechar um tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+$txt['whotopic_mergetopics'] = 'A juntar um tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot; com outro tópico.';
+$txt['whotopic_movetopic'] = 'A mover um tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot; para outro fórum.';
+$txt['whotopic_movetopic2'] = 'A mover o tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot; para outro fórum.';
+$txt['whotopic_post'] = 'A publicar em <a href="<URL>?topic=%1$d.0">%2$s</a>.';
+$txt['whotopic_post2'] = 'A publicar em <a href="<URL>?topic=%1$d.0">%2$s</a>.';
+$txt['whotopic_printpage'] = 'A imprimir o tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+$txt['whotopic_quickmod2'] = 'A moderar o tópico <a href="<URL>?topic=%1$d.0">%2$s</a>.';
+$txt['whotopic_removetopic2'] = 'A remover o tópico <a href="<URL>?topic=%1$d.0">%2$s</a>.';
+$txt['whotopic_sendtopic'] = 'A enviar o tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot; a um amigo.';
+$txt['whotopic_splittopics'] = 'A dividir um tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot; em dois tópicos.';
+$txt['whotopic_pin'] = 'A fixar um tópico &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+
+$txt['whotopic_poll_editpoll'] = 'A editar a votação em &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+$txt['whotopic_poll_editpoll2'] = 'A editar a votaçao em &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+$txt['whotopic_poll_lockvoting'] = 'A fechar a votação em &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+$txt['whotopic_poll_removepoll'] = 'A remover a votação em &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+$txt['whotopic_poll_vote'] = 'A votar em <a href="<URL>?topic=%1$d.0">%2$s</a>.';
+
+$txt['whopost_quotefast'] = 'A citar uma publicação em &quot;<a href="<URL>?topic=%1$d.0">%2$s</a>&quot;.';
+
+$txt['whoadmin_editagreement'] = 'A editar os termos de utilização.';
+$txt['whoadmin_featuresettings'] = 'A editar as definições do fórum.';
+$txt['whoadmin_modlog'] = 'A visualizar os registos de moderação.';
+$txt['whoadmin_serversettings'] = 'A editar as definições do fórum.';
+$txt['whoadmin_permissions'] = 'A editar as permissões do fórum.';
+$txt['whoadmin_theme'] = 'A editar as definições do tema.';
+$txt['whoadmin_trackip'] = 'A seguir um endereço de IP.';
+
+$txt['whoallow_manageboards'] = 'A editar as definições do fórum e das categorias.';
+$txt['whoallow_admin'] = 'A visualizar  o <a href="<URL>?action=admin">Centro de Administração</a>.';
+$txt['whoallow_ban'] = 'A editar a lista de banidos.';
+$txt['whoallow_boardrecount'] = 'A recontar os totais do fórum.';
+$txt['whoallow_editnews'] = 'A editar as novidades.';
+$txt['whoallow_mailing'] = 'A enviar um email do fórum.';
+$txt['whoallow_maintain'] = 'A realizar a rotina de manutenção do fórum.';
+$txt['whoallow_manageattachments'] = 'A gerir os anexos.';
+$txt['whoallow_mlist'] = 'A visualizar a <a href="<URL>?action=mlist">lista de membros</a>.';
+$txt['whoallow_moderate'] = 'A visualizar o <a href="<URL>?action=moderate">centro de moderação</a>.';
+$txt['whoallow_optimizetables'] = 'A otimizar as tabelas da base de dados.';
+$txt['whoallow_repairboards'] = 'A reparar as tabelas da base de dados.';
+$txt['whoallow_search'] = '<a href="<URL>?action=search">Procurar</a> no fórum.';
+$txt['whoallow_search2'] = 'A visualizar os resultados de uma procura.';
+$txt['whoallow_setcensor'] = 'A editar os textos censurados.';
+$txt['whoallow_stats'] = 'A visualizar as <a href="<URL>?action=stats">estatísticas do fórum</a>.';
+$txt['whoallow_viewErrorLog'] = 'A visualizar o registo de erros.';
+$txt['whoallow_viewmembers'] = 'A visualizar a lista de membros.';
+
+$txt['who_topic'] = 'A visualizar o tópico <a href="<URL>?topic=%1$d.0">%2$s</a>.';
+$txt['who_board'] = 'A visualizar o fórum <a href="<URL>?board=%1$d.0">%2$s</a>.';
+$txt['who_index'] = 'A visualizar a página ínicial de <a href="<URL>">{forum_name}</a>.';
+$txt['who_viewprofile'] = 'A visualizar o perfil de <a href="<URL>?action=profile;u=%1$d">%2$s</a>.';
+$txt['who_profile'] = 'A editar o perfil de <a href="<URL>?action=profile;u=%1$d">%2$s</a>.';
+$txt['who_post'] = 'A publicar um novo tópico em <a href="<URL>?board=%1$d.0">%2$s</a>.';
+$txt['who_poll'] = 'A publicar uma nova votação em <a href="<URL>?board=%1$d.0">%2$s</a>.';
+
+// Media area actions
+$txt['media_wo_home'] = 'A visualizar a <a href="<URL>?action=media">Galeria principal</a>';
+$txt['media_wo_admin'] = 'A administrar a galeria';
+$txt['media_wo_unseen'] = 'A visualizar items na galeria por ver';
+$txt['media_wo_search'] = 'A procurar na galeria';
+$txt['media_wo_item'] = 'A visualizar &quot;<a href="<URL>?action=media;sa=item;in=%s">%s</a>&quot; na galeria';
+$txt['media_wo_album'] = 'A visualizar o álbum &quot;<a href="<URL>?action=media;sa=album;in=%s">%s</a>&quot; na galeria';
+$txt['media_wo_add'] = 'A visualizar um item no álbum &quot;<a href="<URL>?action=media;sa=album;in=%s">%s</a>&quot; na galeria';
+$txt['media_wo_edit'] = 'A editar &quot;<a href="<URL>?action=media;sa=item;in=%s">%s</a>&quot; na galeria';
+$txt['media_wo_comment'] = 'A comentar &quot;<a href="<URL>?action=media;sa=item;in=%s">%s</a>&quot; na galeria';
+$txt['media_wo_reporting'] = 'A reportar &quot;<a href="<URL>?action=media;sa=item;in=%s">%s</a>&quot; na galeria';
+$txt['media_wo_stats'] = 'A visualizar as estatísticas da galeria';
+$txt['media_wo_vua'] = 'A visualizar o paínel de controlo de um álbum na galeria';
+$txt['media_wo_ua'] = 'A visualizar a página ínicial de um álbum na galeria';
+$txt['media_wo_unknown'] = 'A executar ações desconhecidas na galeria';
+$txt['media_wo_hidden'] = 'Num sítio obscuro na galeria... assuatador ah?';
+
+// Credits text
+$txt['credits_site'] = 'A equipa';
+$txt['credits_admins'] = array(1 => 'Administrador', 'n' => 'Administradores');
+$txt['credits_moderators'] = array(1 => 'Moderador', 'n' => 'Moderadores');
+$txt['credits_software'] = 'A equipa Wedge (motor do fórum)';
+$txt['credits_wedge'] = 'Wedge direitos de autor © %3$d by %1$s, todos os direitos reservados. É distribuído sobre a licença <a href="%2$s">Wedge license</a>.';
+$txt['credits_smf2'] = 'Usa porções de <a href="http://www.simplemachines.org/">SMF 2.0</a>, copyright © Simple Machines 2011, todos os direitos reservados. SMF é distribuído sobe a licença <a href="http://www.simplemachines.org/about/smf/license.php">BSD</a>.';
+$txt['credits_aeme'] = 'Usa porções de <a href="%4$s">Aeva Media</a>, copyright © 2007-2011 %1$s, %2$s e %3$s.';
+$txt['credits_special'] = 'Especial agradecimentos';
+$txt['credits_copyright'] = 'Copyrights';
+$txt['credits_plugins'] = 'Plugins';
+$txt['credits_groups_ps'] = 'Fundadores';
+$txt['credits_groups_dev'] = 'Desenvolvedores';
+$txt['credits_groups_contributors'] = 'Contribuídores';
+
+// Uncomment the following string, and replace English with the name of your language.
+$txt['credits_groups_language'] = 'Tradução em português de Portugal';
+
+// List of people who have made more than a token contribution to this translation. (See Who.french.php for an example.)
+$txt['translation_credits'] = array(
+	'Tradução em português de Portugal por <a href="http://wedge.org/profile/kimikelku/"target="_blank">kimikelku</a>. Copyright © 2014. Versão em constantes revisões.
+);
