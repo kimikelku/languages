59185b1cc5a204ea9879ab03cfa048e44e49edf4
Ricardo <sismov7db@gmail.com>
2014-01-14 17:57:35 +0000

    Small files added
    Starting to translate the smaller ones

---
+++ portuguese-pt/Boards.portuguese-pt.php
@@ -0,0 +1,35 @@
+<?php
+// Version: 2.0; Boards
+
+$txt['view_unread_category'] = 'Publicações por ler';
+
+// And now for the moderation 'boards'
+$txt['moderation_cat'] = 'Moderação';
+$txt['reported_posts'] = 'Publicações reportadas';
+$txt['reported_board_desc'] = '<strong>Reported</strong> por {author}<br>em {link}<br>{time}';
+$txt['reported_open'] = array(
+	1 => '1 denúncia aberta',
+	'n' => '%1$s denúncias abertas',
+);
+$txt['reported_closed'] = array(
+	1 => '1 denúncia fechada',
+	'n' => '%1$s denúncias fechadas',
+);
+$txt['group_requests'] = 'Pedidos para grupos';
+$txt['group_request_desc'] = 'A juntar-se ao <strong>{groupname}</strong><br>por {author}<br>{time}';
+$txt['group_req_open'] = array(
+	1 => '1 pedido aberto',
+	'n' => '%1$s pedidos abertos',
+);
+$txt['group_req_closed'] = array(
+	1 => '1 pedido fechado',
+	'n' => '%1$s pedidos fechados',
+);
+$txt['logs_board'] = 'Registos';
+$txt['logs_moderation'] = 'Registos de Moderação';
+$txt['logs_infractions'] = 'Registos de Infrações';
+$txt['error_log_count'] = array(
+	0 => 'Sem erros',
+	1 => '1 erro',
+	'n' => '%1$s erros',
+);
---
+++ portuguese-pt/Home.portuguese-pt.php
@@ -0,0 +1,16 @@
+<?php
+// Version: 2.0; Home
+// This is a sample file.
+
+$txt['home_title'] = 'Isto somos nós.';
+$txt['home_intro'] = '
+			Isto fórum utiliza o Wedge, uma plataforma de construção de fóruns desenvolvido em PHP 5.
+			<br><br>
+			<em>De forma a editar esta sinopse, deve clonar os seguintes ficheiros:
+			Sources\Home.php, Templates\Home.template.php, languages\Home.*.php.
+			Adicione-lhes um prefixo diferente, como por exemplo MyHome. Aplique as suas modificações
+			a estes novos ficheiros, faça o upload deles, e configure o Wedge para tornar o novo ficheiro
+			do índice para MyHome (índice declarado por defeito nas definições de tabela).
+			<br>
+			Embora possa parecer complicado, irá permitir-lhe fornecer várias traduções desta sinopse,
+			e de forma segura modificar os ficheiros sem que estes mesmos sejam substituídos quando atualizar o Wedge.</em>';
