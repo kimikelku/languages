6d40936076cdc83f66ec2d57e9986bc5f7a6b76e
Ricardo <sismov7db@gmail.com>
2014-01-18 15:11:50 +0000

    2 new files added
    31 more files to go

---
+++ portuguese-pt/ManageScheduledTasks.portuguese-pt.php
@@ -0,0 +1,53 @@
+<?php
+// Version: 2.0; ManageScheduledTasks
+
+$txt['scheduled_tasks_title'] = 'Tarefas Agendadas';
+$txt['scheduled_tasks_header'] = 'Todas as Tarefas Agendadas';
+$txt['scheduled_tasks_name'] = 'Nome da Tarefa';
+$txt['scheduled_tasks_next_time'] = 'Próxima tarefa';
+$txt['scheduled_tasks_regularity'] = 'Regularidade';
+$txt['scheduled_tasks_enabled'] = 'Ativo';
+$txt['scheduled_tasks_run_now'] = 'Iniciar agora';
+$txt['scheduled_tasks_save_changes'] = 'Guardar alterações';
+$txt['scheduled_tasks_time_offset'] = '<strong>Nota:</strong> Todas as horas apresentadas a baixo estão de acordo com as <em>horas do servidor</em> e não é levado em conta qualquer diferença horária do Wedge.';
+$txt['scheduled_tasks_were_run'] = 'Todas as tarefas selecionadas foram completadas';
+
+$txt['scheduled_tasks_na'] = 'N/A';
+$txt['scheduled_task_approval_notification'] = 'Aprovação de Notificações';
+$txt['scheduled_task_desc_approval_notification'] = 'Enviar a todos os moderadores sumários do estado de publicações por aprovar.';
+$txt['scheduled_task_auto_optimize'] = 'Optimizar a base de dados';
+$txt['scheduled_task_desc_auto_optimize'] = 'Optimizar a base de dados para resolver problemas de fragmentação.';
+$txt['scheduled_task_daily_maintenance'] = 'Manutenção diária';
+$txt['scheduled_task_desc_daily_maintenance'] = 'Executa manutenção diária necessária ao fórum -  não deve de ser desativada.';
+$txt['scheduled_task_daily_digest'] = 'Sumário de notificações diárias';
+$txt['scheduled_task_desc_daily_digest'] = 'Envia o email de notificações diárias aos subscritores.';
+$txt['scheduled_task_weekly_digest'] = 'Sumário de notificações semanais';
+$txt['scheduled_task_desc_weekly_digest'] = 'Envia o email de notificações semanais aos subscritores.';
+$txt['scheduled_task_fetchRemoteFiles'] = 'Procura ficheiros remotos do Wedge';
+$txt['scheduled_task_desc_fetchRemoteFiles'] = 'Recupera ficheiros JavaScript do site oficial, quem contêm notificações de atualizações e outras informações.';
+$txt['scheduled_task_weekly_maintenance'] = 'Manutenção semanal';
+$txt['scheduled_task_desc_weekly_maintenance'] = 'Executa manutenção semanal necessária ao fórum -  não deve de ser desativada.';
+$txt['scheduled_task_paid_subscriptions'] = 'Verificação de subscritores pagos';
+$txt['scheduled_task_desc_paid_subscriptions'] = 'Envia lembranças, aos subscritores pagos, necessárias e remove subscritores expirados.';
+
+$txt['scheduled_task_reg_starting'] = 'A começar a %1$s';
+$txt['scheduled_task_reg_repeating'] = 'A repetir todos %1$d %2$s';
+$txt['scheduled_task_reg_unit_m'] = 'minuto(s)';
+$txt['scheduled_task_reg_unit_h'] = 'hora(s)';
+$txt['scheduled_task_reg_unit_d'] = 'dia(s)';
+$txt['scheduled_task_reg_unit_w'] = 'semana(s)';
+
+$txt['scheduled_task_edit'] = 'Editar Tarefa Agendada';
+$txt['scheduled_task_edit_repeat'] = 'Repetir tarefa todos os';
+$txt['scheduled_task_edit_pick_unit'] = 'Escolher unidade';
+$txt['scheduled_task_edit_interval'] = 'Intervalo';
+$txt['scheduled_task_edit_start_time'] = 'Tempo de início';
+$txt['scheduled_task_edit_start_time_desc'] = 'Hora da execução da primeira instância do dia a que deve de iniciar (horas:minutos)';
+$txt['scheduled_task_time_offset'] = 'Tenha em atenção que deve compensar o tempo em relação à hora do servidor. Hora atual do servidor: %1$s';
+
+$txt['scheduled_view_log'] = 'Visualizar registo';
+$txt['scheduled_log_empty'] = 'De momento não existem entradas no registo de tarefas.';
+$txt['scheduled_log_time_run'] = 'Tempo executado';
+$txt['scheduled_log_time_taken'] = 'Tempo de execução';
+$txt['scheduled_log_time_taken_seconds'] = '%1$d segundos';
+$txt['scheduled_log_empty_log'] = 'Limpar registo';
---
+++ portuguese-pt/ManageTopics.portuguese-pt.php
@@ -0,0 +1,61 @@
+<?php
+// Version: 2.0; ManageTopics
+
+$txt['merge_desc'] = 'Esta função irá juntar as mensagens dos dois tópicos num só. As mensagens serão ordenadas de acordo com a hora a que foram publicadas. Assim sendo a publicação mais antiga será a primeira publicação a ser exibida no novo tópico.';
+$txt['split'] = 'Divir o tópico atual num outro';
+$txt['merge_to_topic_id'] = 'ID do tópico desejado';
+$txt['subject_new_topic'] = 'Assunto para o Novo Tópico';
+$txt['split_this_post'] = 'Apenas separar esta publicação.';
+$txt['split_after_and_this_post'] = 'Separar o tópico apenas com esta publicação e as seguintes.';
+$txt['select_split_posts'] = 'Selecionar publicações a separar.';
+$txt['board_for_new_topic'] = 'Qual é o fórum para onde deve de ir o novo tópico?';
+$txt['please_select_split'] = 'Por favor selecione as publicações que deseja de separar.';
+$txt['merge_successful'] = 'Tópicos fundidos com sucesso.';
+$txt['split_successful'] = 'Tópico separado em dois com sucesso.';
+$txt['origin_topic'] = 'Tópico de origem';
+$txt['new_merged_topic'] = 'Tópico fundido recentemente';
+$txt['topic_to_merge'] = 'Tópico a ser fundido';
+$txt['target_board'] = 'Fórum escolhido';
+$txt['target_topic'] = 'Tópico escolhido';
+$txt['split_selected_posts'] = 'Publicações selecionadas';
+$txt['split_selected_posts_desc'] = 'As publicações a baixo formaram um novo tópico após a separação.';
+$txt['split_reset_selection'] = 'redefinir seleção';
+
+$txt['moved'] = 'MOVIDO';
+$txt['moved_why'] = 'Por favor insira uma pequena descrição da razão<br>pelo qual vai mover este tópico.';
+$txt['moveTopic1'] = 'Publicar o tópico de redirecionamento.';
+$txt['moveTopic2'] = 'Modificar o assunto do tópico.';
+$txt['moveTopic3'] = 'Novo assunto';
+$txt['moveTopic4'] = 'Modificar o assunto de todas as mensagens.';
+$txt['moveTopic_redirection_period'] = 'Durante quanto tempo deve o <br>tópico de redirecionamento manter-se?';
+$txt['moveTopic_redirection_day'] = array(1 => '1 dia', 'n' => '%s dias');
+$txt['moveTopic_redirection_perm'] = 'Permanentemente';
+$txt['moveTopic_redirection_auto'] = 'Devem os utilizadores ser redirecionados automáticamente para o novo tópico?';
+$txt['move_topic_unapproved_js'] = 'Aviso! Este tópico ainda não foi aprovado.<br><br>Não é recomendado aprovar as publicações logo após mover o tópico.';
+$txt['movetopic_auto_board'] = '[FÓRUM]';
+$txt['movetopic_auto_topic'] = '[LINK DO TÓPICO]';
+$txt['movetopic_default'] = 'este tópico foi movido para {auto_board}' . "\n\n" . '{auto_topic}';
+
+$txt['sendtopic_send'] = 'Enviar';
+$txt['sendtopic_title'] = 'Enviar o tópico &quot;%1$s&quot; a um amigo.';
+$txt['sendtopic_sender_name'] = 'O seu nome';
+$txt['sendtopic_sender_email'] = 'O seu endereço de email';
+$txt['sendtopic_receiver_name'] = 'Nome do destinatário';
+$txt['sendtopic_receiver_email'] = 'Endereço de email do destinatário';
+$txt['sendtopic_comment'] = 'Adicionar um comentário';
+
+$txt['merge_select_target_board'] = 'Selecionar o fórum escolhido para o tópico fundido';
+$txt['merge_select_poll'] = 'Selecione a votação que o tópico fundido deve de ter';
+$txt['merge_topic_list'] = 'Selecione os tópcios a serem fundidos';
+$txt['merge_select_subject'] = 'Selecione o assunto do tópico fundido';
+$txt['merge_custom_subject'] = 'Assunto personalizado';
+$txt['merge_enforce_subject'] = 'Modificar o assunto de todas as mensagens';
+$txt['merge_include_notifications'] = 'Incluír notificações?';
+$txt['merge_check'] = 'Fundir?';
+$txt['merge_no_poll'] = 'Sem votação';
+
+$txt['report_to_mod_func'] = 'Usar esta função para informar os moderadores e administradores de uma mensagem abusiva ou publicada erradamente.<br><em>Tenha em atenção que o seu email será revelado aos moderadores ao utilizar esta função.</em>';
+
+$txt['send_email'] = 'Enviar um email';
+$txt['send_email_disclosed'] = 'Tenha em atenção que isto será visivel ao destinatário.';
+$txt['send_email_subject'] = 'Assunto do email';
