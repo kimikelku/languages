5ee35b2d56caf0c5661d1fe9c76ff9aa659dcc41
Ricardo <sismov7db@gmail.com>
2014-03-01 13:52:15 +0000

    Some Strings updated and files reviewed

---
+++ portuguese-pt/ManageSmileys.portuguese-pt.php
@@ -0,0 +1,90 @@
+<?php
+// Version: 2.0; ManageSmileys
+
+$txt['smiley_sets_save'] = 'Guardar alterações';
+$txt['smiley_sets_add'] = 'Novo conjunto de Smileys';
+$txt['smiley_sets_delete'] = 'Remover seleção';
+$txt['smiley_sets_confirm'] = 'Tem a certeza que pretende remover este conjunto de smileys?<br><br>Nota: Não irá remover as imagens, apenas a possibilidade de escolha.';
+
+$txt['setting_smiley_sets_default'] = 'Conjunto de Smileys padrão';
+$txt['setting_smiley_sets_enable'] = 'Ativar seleção de conjuntos de smileys pelos utilizadores';
+$txt['setting_smiley_enable'] = 'Organizar, adicionar e editar smileys manualmente';
+$txt['setting_smileys_url'] = 'URL base para todos os conuntos de smileys';
+$txt['setting_smileys_dir'] = 'Caminho absoluto para todos os conuntos de smileys';
+$txt['groups_manage_smileys'] = 'Grupos com permissão para  gerir smileys e ícones de mensagens';
+
+$txt['smiley_sets_name'] = 'Nome';
+$txt['smiley_sets_url'] = 'URL';
+$txt['smiley_sets_default'] = 'Padrão';
+
+$txt['smiley_sets_latest'] = 'Smileys do Momento';
+$txt['smiley_sets_latest_fetch'] = 'Carregar informação de smiley de wedge.org...';
+
+$txt['smileys_add_method'] = 'Fonte da Imagem';
+$txt['smileys_add_existing'] = 'Usar ficheiro existente';
+$txt['smileys_add_upload'] = 'Enviar Novo Smiley';
+$txt['smileys_add_upload_choose'] = 'Ficheiro a Enviar';
+$txt['smileys_add_upload_choose_desc'] = 'Imagem a ser utilizada por todos os conjuntos de smileys.';
+$txt['smileys_add_upload_all'] = 'Mesma imagem para todos os conjuntos';
+$txt['smileys_add_upload_for1'] = 'Imagem para';
+$txt['smileys_add_upload_for2'] = 'conjunto';
+
+$txt['smileys_enable_subtext'] = '(caso contrário, os smileys padrão serão utilizados.)';
+$txt['smileys_code'] = 'Código';
+$txt['smileys_filename'] = 'Nome do ficheiro';
+$txt['smileys_description'] = 'Dica ou descrição';
+$txt['smileys_save'] = 'Guardar alterações';
+$txt['smileys_delete'] = 'Remover Smiley';
+$txt['smileys_delete_confirm'] = 'Tem a certeza que pretende remover este smiley?';
+$txt['smileys_with_selected'] = 'Com os Selecionados';
+$txt['smileys_make_hidden'] = 'Ocultá-lo';
+$txt['smileys_show_on_post'] = 'Exibir na janela de mensagem';
+$txt['smileys_show_on_popup'] = 'Exibir na Janela Flutuante';
+
+$txt['smiley_settings_explain'] = 'Estas configurações permitem-lhe modificar o conjunto de smileys padrão, permite aos utilizadores escolher os próprios smileys, adicionar caminhos e configurações.';
+$txt['smiley_editsets_explain'] = 'Conuntos de Smileys são grupos de smileys que os seus utilizadores podem escolher. Por exemplo, pode ter smileys amarelos e vermelhos.<br>Aqui pode modificar o nome e o caminho de cada conjunto de smileys - lembre-se, no entanto, que cada conjunto partilha os mesmos smileys.';
+$txt['smiley_editsmileys_explain'] = 'Altere os seus smileys aqui clicando no smiley que pretende modificar. Lembre-se que todos estes smileys devem de existir em todos os conjuntos, caso contrário não serão exibidos! Não se esqueça de guardar depois de alterar!';
+$txt['smiley_setorder_explain'] = 'Alterar a ordem dos smileys aqui.';
+$txt['smiley_addsmiley_explain'] = 'Aqui pode adicionar um novo smiley - a partir de um ficheiro existente ou enviando um novo.';
+
+$txt['smiley_set_select_default'] = 'Conjunto de Smileys Padrão';
+$txt['smiley_set_new'] = 'Criar novo conjunto de smileys';
+$txt['smiley_set_modify_existing'] = 'Modificar conjunto de smileys existente';
+$txt['smiley_set_modify'] = 'Modificar';
+$txt['smiley_set_import_directory'] = 'Importar smileys já existentes neste diretório';
+$txt['smiley_set_import_single'] = 'Existe um smiley neste conjunto que ainda não foi importado. Clique';
+$txt['smiley_set_import_multiple'] = 'Existem %1$d smileys no diretório que ainda não foram importados. Clique';
+$txt['smiley_set_to_import_single'] = 'para importar agora.';
+$txt['smiley_set_to_import_multiple'] = 'para importá-los agora.';
+
+$txt['smileys_location'] = 'Localização';
+$txt['smileys_location_form'] = 'Janela de mensagem';
+$txt['smileys_location_hidden'] = 'Ocultar';
+$txt['smileys_location_popup'] = 'Janela Flutuante';
+$txt['smileys_modify'] = 'Modificar';
+$txt['smileys_not_found_in_set'] = 'Smiley não encontrado no conjunto(s)';
+$txt['smileys_default_description'] = '(Inserir uma descrição)';
+$txt['smiley_new'] = 'Adicionar novo smiley';
+$txt['smiley_modify_existing'] = 'Modificar smiley';
+$txt['smiley_preview'] = 'Pré-visualizar';
+$txt['smiley_preview_using'] = 'a usar conjunto de smileys';
+$txt['smileys_confirm'] = 'Tem a certeza que pretende remover estes smileys?<br><br>Nota: Não irá remover as imagens, apenas a possibilidade de escolha.';
+$txt['smileys_location_form_description'] = 'Estes smileys irão ser apresentados a cima da área de texto, quando publicar uma nova mensagem ou Mensagem Pessoal.';
+$txt['smileys_location_popup_description'] = 'Estes smileys serão apresentados numa janela flutuante, que será apresentados após um utilizador clicar em \'[mais]\'';
+$txt['smileys_move_select_destination'] = 'Selecionar destino do smiley';
+$txt['smileys_move_select_smiley'] = 'Selecionar smiley a ser movido';
+$txt['smileys_move_here'] = 'Mover smiley para esta localização';
+$txt['smileys_no_entries'] = 'De momento não exitem smileys configurados.';
+
+$txt['icons_edit_icons_explain'] = 'A partir de aqui pode modificar que ícones de mensagens serão disponibilizados em todo o seu fórum. Pode adicionar, editar e remover ícones, e também limitar o seu uso no fórum.';
+$txt['icons_edit_icons_all_boards'] = 'Disponível em Todos os Fórums';
+$txt['icons_board'] = 'Fórum';
+$txt['icons_confirm'] = 'Tem a certeza que pretende remover estes ícones?<br><br>Tenha em atenção que esta ação apenas irá impedir que novas publicações utilizem estes ícones, as imagens manter-se-ão.';
+$txt['icons_add_new'] = 'Adicionar novo Ícone';
+
+$txt['icons_edit_icon'] = 'Editar ícone de mensagem';
+$txt['icons_new_icon'] = 'Novo ícone de mensagem';
+$txt['icons_location_first_icon'] = 'Como Primeiro Ícone';
+$txt['icons_location_after'] = 'Depois';
+$txt['icons_filename_all_gif'] = 'Todos os ficheiros devem de ser &quot;gif&quot;';
+$txt['icons_no_entries'] = 'De momento não exitem ícones de mensagens configurados.';
---
+++ portuguese-pt/Themes.portuguese-pt.php
@@ -0,0 +1,85 @@
+<?php
+// Version: 2.0; Themes
+
+$txt['themeadmin_explain'] = 'Os temas regem a forma em como o seu fórum é apresentado. Estas configurações modificam a seleção dos temas, e os temas que os utilizadores e convidados usam.';
+
+$txt['theme_allow'] = 'Permitir que os utilizadores escolham as skins?';
+$txt['theme_guests'] = 'Padrão geral para os convidados';
+$txt['theme_guests_mobile'] = 'Padrão geral para os convidados (modo telemóvel)';
+$txt['theme_select'] = 'escolher...';
+$txt['theme_reset'] = 'Repor a todos para';
+$txt['theme_nochange'] = 'Não modificar';
+$txt['theme_forum_default'] = 'Predefinido do fórum';
+
+$txt['theme_remove_confirm'] = 'Tem a certeza que pretende remover permanentemente este tema?';
+
+$txt['theme_install'] = 'Instalar um novo tema';
+$txt['theme_install_file'] = 'A partir de um ficheiro';
+$txt['theme_install_dir'] = 'A partir de um diretório do servidor';
+$txt['theme_install_error'] = 'O diretório desse tema não existe ou não contém um tema!';
+$txt['theme_install_write_error'] = 'O diretório deve de ter permissões de escrita para continuar!';
+$txt['theme_install_go'] = 'Instalar!';
+$txt['theme_install_new'] = 'Ou criar uma cópia do tema padrão com o nome';
+$txt['theme_install_new_confirm'] = 'Tem a certeza que pretende instalar este novo tema?';
+$txt['theme_install_writable'] = 'Aviso - não pode instalar ou criar um tema porque o diretório do seu tema não tem permissões de escrita!';
+$txt['theme_installed'] = 'Instalado com sucesso';
+$txt['theme_installed_message'] = 'foi instalado com sucesso.';
+
+$txt['theme_latest'] = 'Últimos e principais temas';
+$txt['theme_latest_fetch'] = 'A procurar os temas mais recentes de wedge.org...';
+
+$txt['theme_set'] = 'Usar o estilo padrão';
+$txt['skin_users'] = array(
+	0 => 'Ninguém está a usar este skin.',
+	1 => '1 pessoa está a usar este skin.',
+	'n' => '%s estão a usar este skin.',
+);
+$txt['current_theme'] = 'Tema de momento';
+
+$txt['theme_skins'] = 'Skins';
+$txt['theme_skin_preview'] = 'Pré-visualizar skin';
+$txt['theme_skin_set'] = 'Usar este skin';
+
+$txt['theme_edit'] = 'Editar Tema';
+$txt['theme_edit_style'] = 'Modificar os stylesheets. (Cores, fontes, etc.)';
+$txt['theme_edit_index'] = 'Modificar o modelo de do index. (O modelo principal)';
+$txt['theme_edit_no_save'] = 'Este ficheiro não pode ser salvo porque não tem permissões de escrita! Por favor verifique se o ficheiro tem permissões 777';
+$txt['theme_edit_save'] = 'Guardar alterações';
+
+$txt['theme_global_description'] = 'Este é o tema padrão, o que significa que irá será modificado da mesma forma que as configurações dos administradores e do fórum que se encontra a visualizar.';
+
+$txt['themeadmin_title'] = 'Configurações dos temas e disposições';
+$txt['themeadmin_description'] = 'Aqui pode modificar as configurações dos seus temas, atualizar as seleções dos temas, redefinir as opções dos membros, e os estilos.';
+$txt['themeadmin_admin_desc'] = 'Esta página permite-lhe modificar o tema padrão, redefinir todos os membros a usar um tema, e escolher outras configurações relacionadas com a seleção de temas. Tem ainda a possibilidade de instalar temas.<br><br>Não se esqueça de verificar as configurações do tema e da disposição.';
+$txt['themeadmin_list_desc'] = 'A partir de aqui, pode visualizar a lista de temas instalados, modificar os seus caminhos e configurações, e desinstalá-los.';
+$txt['themeadmin_edit_desc'] = 'Modificar o stylesheet e o código fonte dos temas instalados. Por favor consulte a documentação para mais informação.';
+
+$txt['themeadmin_list_heading'] = 'Configurações gerais dos temas';
+$txt['themeadmin_list_tip'] = 'Lembre-se, as configurações da disposição podem diferenciar entre um tema para o outro. Clique no nome do tema a baixo e aplique as definições, modifique o seu diretório ou as configurações do URL, ou encontre outras opções.';
+$txt['themeadmin_list_theme_dir'] = 'Diretório dos temas (modelos)';
+$txt['themeadmin_list_invalid'] = '(aviso, este caminho não se encontra correto!)';
+$txt['themeadmin_list_theme_url'] = 'URL para o diretório a cima';
+$txt['themeadmin_list_reset'] = 'Repor o URL dos temas e diretórios';
+$txt['themeadmin_list_reset_dir'] = 'Caminho base para o diretório dos temas';
+$txt['themeadmin_list_reset_url'] = 'URL base para o mesmo diretório';
+$txt['themeadmin_list_reset_go'] = 'Tentar repor todos os temas';
+
+$txt['themeadmin_edit_browse'] = 'Navegar pelos ficheiros e modelos deste tema.';
+$txt['themeadmin_edit_style'] = 'Navegar pelos stylesheets deste tema.';
+$txt['themeadmin_edit_copy_template'] = 'Copiar o modelo em que este tema de baseia.';
+$txt['themeadmin_edit_exists'] = 'já existe';
+$txt['themeadmin_edit_do_copy'] = 'copiar';
+$txt['themeadmin_edit_copy_warning'] = 'Quando o  Wedge necessita de um modelo ou de um ficheiro de língua que não se encontre no tema escolhido, ele procurará no tema em que é baseado, ou no tema padrão.<br>A não ser que precise de modificar o modelo, é melhor não o copiar.';
+$txt['themeadmin_edit_copy_confirm'] = 'Tem a certeza que pretende copiar este modelo?';
+$txt['themeadmin_edit_overwrite_confirm'] = 'Tem a certeza que pretende copiar este modelo pelo que já existe?<br>Irá SUBSTITUIR qualquer modificação que tenha feito!';
+$txt['themeadmin_edit_no_copy'] = '<em>(não pode copiar)</em>';
+$txt['themeadmin_edit_filename'] = 'nome do ficheiro';
+$txt['themeadmin_edit_modified'] = 'Última vez modificado';
+$txt['themeadmin_edit_size'] = 'Tamanho';
+$txt['themeadmin_edit_bytes'] = 'B';
+$txt['themeadmin_edit_kilobytes'] = 'KB';
+$txt['themeadmin_edit_error'] = 'O ficheiro que tentou guardar gerou o seguinte erro:';
+$txt['themeadmin_edit_on_line'] = 'A começar na linha';
+$txt['themeadmin_edit_preview'] = 'Pré-visualizar';
+$txt['themeadmin_selectable'] = 'Temas que o utilizador pode escolher';
+$txt['themeadmin_themelist_link'] = 'Mostrar a lista de temas';
