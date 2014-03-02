863a2c122bca5628571449843ad248af2629bfad
Ricardo <sismov7db@gmail.com>
2014-03-02 19:32:53 +0000

    Reviewed file and ready to be uploaded

---
+++ portuguese-pt/Post.portuguese-pt.php
@@ -0,0 +1,218 @@
+<?php
+// Version: 2.0; Post
+
+$txt['post_reply'] = 'Publicar Resposta';
+$txt['enter_verification_details'] = 'Completar verificação de detalhes';
+$txt['message_icon'] = 'Ícone de Mensagem';
+$txt['current_icon'] = 'Ícone de Momento';
+$txt['subject_not_filled'] = 'O Campo do Assunto não foi preenchido. É necessário.';
+$txt['message_body_not_filled'] = 'O Corpo da Mensgaem não foi preenchido. É necessário.';
+
+$txt['add_bbc'] = 'Adicionar tags BBC';
+$txt['bold'] = 'Negrito';
+$txt['italic'] = 'Itálico';
+$txt['underline'] = 'Sublinhado';
+$txt['center'] = 'Centrado';
+$txt['hyperlink'] = 'Inserir Hiperligação';
+$txt['insert_email'] = 'Inserir Email';
+$txt['bbc_code'] = 'Inserir Código';
+$txt['bbc_spoiler'] = 'Inserir Spoiler';
+// Escape any single quotes in here twice.. 'it\'s' -> 'it\\\'s'.
+$txt['bbc_quote'] = 'Inserir Citação';
+$txt['list'] = 'Inserir Lista';
+$txt['list_unordered'] = 'Inserir Lista Desordenada';
+$txt['list_ordered'] = 'Inserir Lista Ordenada';
+$txt['footnote'] = 'Nota de Rodapé';
+
+$txt['change_color'] = 'Alterar Cor';
+$txt['black'] = 'Preto';
+$txt['red'] = 'Vermelho';
+$txt['yellow'] = 'Amarelo';
+$txt['pink'] = 'Rosa';
+$txt['green'] = 'Verde';
+$txt['orange'] = 'Laranja';
+$txt['purple'] = 'Roxo';
+$txt['blue'] = 'Azul';
+$txt['beige'] = 'Bege';
+$txt['brown'] = 'Castanho';
+$txt['teal'] = 'Azul Petróleo';
+$txt['navy'] = 'Azul Escuro';
+$txt['maroon'] = 'Castanho Avermelhado';
+$txt['lime_green'] = 'Verde Lima';
+$txt['white'] = 'Branco';
+$txt['disable_smileys'] = 'Desativar Smileys';
+$txt['dont_use_smileys'] = 'Não usar smileys.';
+$txt['posted_on'] = 'Publicado a';
+$txt['standard'] = 'Standard';
+$txt['thumbs_up'] = 'Thumb Up';
+$txt['thumbs_down'] = 'Thumb Down';
+$txt['excamation_point'] = 'Ponto de Exclamação';
+$txt['question_mark'] = 'Ponto de Interrogação';
+$txt['lamp'] = 'Lâmpada';
+$txt['add_smileys'] = 'Adicionar Smileys';
+$txt['media'] = 'Enviar ficheiro Multimédia';
+$txt['flash'] = 'Inserir Flash';
+$txt['ftp'] = 'Inserir Link FTP';
+$txt['image'] = 'Inserir Imagem';
+$txt['table'] = 'Inserir Tabela';
+$txt['table_td'] = 'Inserir Coluna de Tabela';
+$txt['topic_notify_no'] = 'Não existem Tópicos com Notificações.';
+$txt['teletype'] = 'Teletype';
+$txt['strike'] = 'Strikethrough';
+$txt['preformatted'] = 'Texto Pré-formatado';
+$txt['left_align'] = 'Alinhar à Esquerda';
+$txt['right_align'] = 'Alinhar à Esquerda';
+$txt['superscript'] = 'Superior à Linha';
+$txt['subscript'] = 'Inferior à Linha';
+$txt['table_tr'] = 'Inserir Linha de Tabela';
+$txt['post_too_long'] = 'A sua mensagem é demasiado longa. Diminiu-a e volte a tentar.';
+$txt['horizontal_rule'] = 'Linha Horizontal';
+$txt['more_bbc'] = 'Mais (cortar a página neste ponto como por exemplo uma publicação de blog)';
+$txt['font_size'] = 'Tamanho da Letra';
+$txt['font_face'] = 'Tipo de Letra';
+$txt['toggle_view'] = 'Alterar Vista';
+$txt['unformat_text'] = 'Remover Formatação';
+
+$txt['icon_smiley'] = 'Sorriso';
+$txt['icon_angry'] = 'Angry';
+$txt['icon_cheesy'] = 'Cheesy';
+$txt['icon_sad'] = 'Triste';
+$txt['icon_wink'] = 'Wink';
+$txt['icon_grin'] = 'Grin';
+$txt['icon_shocked'] = 'Chocado';
+$txt['icon_cool'] = 'Fixe!';
+$txt['icon_huh'] = 'Huh?';
+$txt['icon_rolleyes'] = 'Roll eyes';
+$txt['icon_tongue'] = 'Língua';
+$txt['icon_embarrassed'] = 'Envergonhado';
+$txt['icon_lips'] = 'Lips sealed';
+$txt['icon_undecided'] = 'Indeciso';
+$txt['icon_kiss'] = 'Beijo';
+$txt['icon_cry'] = 'Chorar';
+$txt['icon_edit'] = 'Editado';
+
+// Editor prompt.
+$txt['prompt_text_email'] = 'Por favor insira o endereço de email.';
+$txt['prompt_text_ftp'] = 'Por favor insira o endereço de FTP.';
+$txt['prompt_text_url'] = 'Por favor insira o URL.';
+$txt['prompt_text_img'] = 'Insira a localização da imagem';
+$txt['prompt_text_desc'] = 'Por favor insira a descrição da hiperligação. Deixe em branco para mostrar o URL.';
+
+$txt['lock_after_post'] = 'Fechar depois de Publicar';
+$txt['notify_replies'] = 'Notificar se responderem.';
+$txt['lock_topic'] = 'Fechar este tópico.';
+$txt['shortcuts'] = 'atalhos: clique <kbd>Shift + Enter</kbd> para fechar uma tag, <kbd>Ctrl + B</kbd> (negrito), <kbd>Ctrl + I</kbd> (itálico), <kbd>Ctrl + U</kbd> (sublinhado), <kbd>Alt + D</kbd> (guardar rascunho), <kbd>Alt + P</kbd> (pré-visualizar) ou <kbd>Alt + S</kbd> para submeter.';
+$txt['option'] = 'Opção';
+$txt['reset_votes'] = 'Redefinir Contagem de Votação';
+$txt['reset_votes_check'] = 'Insira um visto se desejar redefinir as votações para 0.';
+$txt['votes'] = 'votos';
+$txt['attach'] = 'Anexar';
+$txt['attached'] = 'Anexado';
+$txt['allowed_types'] = 'Extensões permitidas';
+$txt['cant_upload_type'] = 'A extensão do ficheiro não é válida. Apenas são permitidos as seguintes extensões';
+$txt['uncheck_unwatchd_attach'] = 'Remova a seleção dos anexos que já não deseja ter anexado';
+$txt['restricted_filename'] = 'O seu ficheiro tem como nome "%1$s" - este nome não é permitido, escolha outro.';
+$txt['topic_locked_no_reply'] = 'Aviso: tópico será/de momento fechado!<br>Apenas Admins e moderadores podem responder.';
+$txt['error_temp_attachments'] = 'Anexos encontrados, que foram enviados mas não publicados. Esses anexos foram agora anexados a esta publicação. Se não quiser incluí-los nesta publicação, pode remove-los <a href="#postAttachment">aqui</a>.';
+$txt['js_post_will_require_approval'] = 'Aviso: Esta publicação não será visivel até que seja aprovada por um moderador.';
+
+$txt['switch_mode'] = 'Alterar para editor completo';
+$txt['enter_comment'] = 'Adicionar comentário';
+
+$txt['attach_restrict_attachmentPostLimit'] = 'tamanho máximo %1$dKB';
+$txt['attach_restrict_attachmentSizeLimit'] = 'tamanho individual máximo %1$dKB';
+$txt['attach_restrict_attachmentNumPerPostLimit'] = '%1$d por publicação';
+$txt['attach_restrictions'] = 'Restrições:';
+
+$txt['post_additionalopt'] = 'Anexos e outas opções&hellip;';
+$txt['pin_after'] = 'Fixar tópico.';
+$txt['move_after2'] = 'Mover tópico.';
+$txt['back_to_topic'] = 'Voltar a este tópico.';
+$txt['approve_this_post'] = 'Aprovar tópico';
+
+$txt['post_visual_verification_label'] = 'Verificação';
+$txt['post_visual_verification_desc'] = 'Por favor insira o código da imagem a cima para publicar.';
+
+$txt['poll_question'] = 'Questão';
+$txt['poll_options'] = 'Opções de votação';
+$txt['poll_run'] = 'Duração da votação até';
+$txt['poll_run_limit'] = '(Em branco, sem limite.)';
+$txt['poll_results_visibility'] = 'Visibilidade do resultado';
+$txt['poll_results_anyone'] = 'Exibir resultados a todos.';
+$txt['poll_results_voted'] = 'Apenas exibir resultados após alguém votar.';
+$txt['poll_results_after'] = 'Apenas exibir resultados depois da votação expirar.';
+$txt['poll_max_votes'] = 'Máximo de votos por utilizador';
+$txt['poll_do_change_vote'] = 'Permitir alteração dos votos pelo utilizador';
+$txt['poll_too_many_votes'] = 'Selecionou demasiadas opçõesYou selected too many options. Nesta votação apenas pode selecionar %1$s opções.';
+$txt['poll_add_option'] = 'Adicionar Opção';
+$txt['poll_guest_vote'] = 'Permitir que convidados votem';
+
+$txt['poll_voters_visibility'] = 'Visibilidade do eleitor';
+$txt['poll_voters_visibility_admin'] = 'Apenas administradores poderão ver quem votou e no quê';
+$txt['poll_voters_visibility_creator'] = 'Os administradores, e o creador da votação poderão ver quem votou e no quê';
+$txt['poll_voters_visibility_members'] = 'Qualquer membro registado poderá ver quem votou e no quê';
+$txt['poll_voters_visibility_anyone'] = 'Qualquer um, até convidados, poderão ver quem votou e no quê';
+$txt['poll_voters_no_change_future'] = 'Não poderá alterar esta opção mais tarde.';
+$txt['poll_voters_no_change_now'] = 'Não pode alterar isto.';
+
+$txt['cannot_attach_ext'] = 'The type of file you have tried to attach ({ext}) is not allowed here. The allowed types of file are: {attach_exts}';
+
+// Don't use entities in the below string.
+$txt['more_attachments_error'] = 'Sorry, you aren\'t allowed to post any more attachments.';
+
+$txt['more_smileys'] = 'mais';
+$txt['more_smileys_pick'] = 'Show available smileys';
+
+$txt['save_draft'] = 'Guardar como rascunho';
+$txt['last_saved_on'] = 'Última vez salvo';
+$txt['draft_saved'] = 'Your post has been saved as a draft. You can access it from <a href="{draft_link}">your profile</a>.';
+$txt['pm_draft_saved'] = 'Your message has been saved as a draft. You can access it from <a href="{draft_link}">your drafts area</a>.';
+$txt['save_draft_warning'] = 'Saving a draft will retain the basic post details (title, text, icon), but not attachments, poll options or anything else. Are you happy to proceed with saving this draft?';
+$txt['empty_draft'] = 'You cannot have a draft that is completely empty.';
+
+$txt['report_sent'] = 'Your report has been sent successfully.';
+
+$txt['deleted_thought'] = '(Removido)';
+
+$txt['error_new_reply'] = array(
+	1 => 'Warning - while you were typing, a new reply has been posted. You may wish to review your post.',
+	'n' => 'Warning - while you were typing, %1$d new replies have been posted. You may wish to review your post.',
+);
+$txt['error_new_reply_reading'] = array(
+	1 => 'Warning - while you were reading, a new reply has been posted. You may wish to review it before posting.',
+	'n' => 'Warning - while you were reading, %1$d new replies have been posted. You may wish to review them before posting.',
+);
+
+$txt['announce_this_topic'] = 'Send an announcement about this topic to the members:';
+$txt['announce_title'] = 'Send an announcement';
+$txt['announce_desc'] = 'This form allows you to send an announcement to the selected membergroups about this topic.';
+$txt['announce_sending'] = 'Sending announcement of topic';
+$txt['announce_done'] = 'done';
+$txt['announce_continue'] = 'Continuar';
+$txt['announce_topic'] = 'Anunciar tópico.';
+$txt['announce_regular_members'] = 'Membros Regulares';
+
+$txt['digest_subject_daily'] = 'Resumo Diário';
+$txt['digest_subject_weekly'] = 'Resumo Semanal';
+$txt['digest_intro_daily'] = 'Abaixo está o sumário de todas as atividades dos fóruns onde se encontra subscrito %1$s até hoje. Para cancelar a subscrição visite o link abaixo.';
+$txt['digest_intro_weekly'] = 'Abaixo está o sumário de todas as atividades dos fóruns onde se encontra subscrito %1$s esta semana. Para cancelar a subscrição visite o link abaixo.';
+$txt['digest_new_topics'] = 'Os seguintes tópicos foram abertos';
+$txt['digest_new_topics_line'] = '"%1$s" em "%2$s"';
+$txt['digest_new_replies'] = 'Respostas publicadas nos seguintes tópicos';
+$txt['digest_new_replies_one'] = '1 resposta em "%1$s"';
+$txt['digest_new_replies_many'] = '%1$d respostas em "%2$s"';
+$txt['digest_mod_actions'] = 'As seguintes ações de moderadores foram realizadas';
+$txt['digest_mod_act_pin'] = '"%1$s" foi fixado';
+$txt['digest_mod_act_lock'] = '"%1$s" foi fechado';
+$txt['digest_mod_act_unlock'] = '"%1$s" foi reaberto';
+$txt['digest_mod_act_remove'] = '"%1$s" foi removido';
+$txt['digest_mod_act_move'] = '"%1$s" foi movido';
+$txt['digest_mod_act_merge'] = '"%1$s" foi junto';
+$txt['digest_mod_act_split'] = '"%1$s" wfoi separado';
+
+$txt['mail_send_unable'] = 'Não foi possivel enviar o mail ao seguinte email \'%1$s\'';
+$txt['topic_summary'] = 'Sumário de Tópico';
+
+$txt['order_pinned_topics'] = 'Ordenar Tópicos Fixados';
+$txt['order_pinned_topics_desc'] = 'Nesta área pode reorganizar os tópicos fixos neste fórum. Apenas arraste-os para as suas novas posições e clique em guardar.';
+$txt['order_last_post_by'] = 'Última publicação por %1$s<br>%2$s';
