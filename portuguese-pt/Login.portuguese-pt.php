b39d1c202178e4d5e010d26534f9a8a01d9504c1
Ricardo <sismov7db@gmail.com>
2014-03-05 18:33:58 +0000

    File Login.english.php reviewed
    Ficheiro Login.english.php revisto

---
+++ portuguese-pt/Login.portuguese-pt.php
@@ -0,0 +1,149 @@
+<?php
+// Version: 2.0; Login
+
+$txt['maintain_mode'] = 'Modo de Manutenção';
+$txt['admin_login'] = 'Autenticação de Administração';
+$txt['username_no_exist'] = 'O utilizador não existe.';
+$txt['no_user_with_email'] = 'Não existem utilizadores associados a esse email.';
+$txt['name_in_use'] = 'Este nome já se encontra a ser utilizado.';
+$txt['mins_logged_in'] = 'Minutos para manter a sessão';
+$txt['always_logged_in'] = 'Permanecer sempre autenticado';
+
+// Registration agreement page.
+$txt['registration_agreement'] = 'Termos de Utilização';
+$txt['registration_reagreement_force'] = 'Os termos de Utilização foram alteradas desde a última vez que as aceitou. De maneira a continuar a utilizar o site, tem de aceitar os novos termos.';
+$txt['registration_reagreement_postonly'] = 'Os termos de Utilização foram alteradas desde a última vez que as aceitou. Pode continuar a navegar pelo site, mas para publicar terá de aceitar os novos termos.';
+$txt['agreement_agree'] = 'Eu aceito os termos de utilização.';
+$txt['agreement_agree_coppa_above'] = 'Eu aceito os termos de utilização e tenho mais de %1$d anos.';
+$txt['agreement_agree_coppa_below'] = 'Eu aceito os termos de utilização e tenho menos de %1$d anos.';
+$txt['noagreement_coppa_above'] = 'Tenho mais de %1$d anos.';
+$txt['noagreement_coppa_below'] = 'Tenho menos de %1$d anos.';
+$txt['agreement_reagreed'] = 'Obrigado por aceitar os termos de utilização atualizados.';
+$txt['agreement_return_to'] = 'Voltar para onde me encontrava a navegar';
+
+// Registration form.
+$txt['registration_form'] = 'Formulário de Registo';
+$txt['need_username'] = 'Tem de preencher o nome de utilizador.';
+$txt['no_password'] = 'Não introduziu a palavra-passe.';
+$txt['incorrect_password'] = 'Palavra-passe incorreta';
+$txt['choose_username'] = 'Escolher nome de utilizador';
+$txt['choose_timezone'] = 'Qual o seu fuso horário?';
+$txt['registration_successful'] = 'Registado com Sucesso!';
+$txt['now_a_member'] = 'Sucesso! É agora um membro do fórum.';
+$txt['email_placeholder'] = 'email@address.com';
+$txt['valid_email_needed'] = 'Por favor introduza um endereço de email válido, %1$s.';
+$txt['required_info'] = 'Informação Necessária';
+$txt['additional_information'] = 'Informação Adicional';
+$txt['warning'] = 'Aviso!';
+$txt['only_members_can_access'] = 'Apenas utilizadores registados podem aceder a esta zona.';
+$txt['login_below'] = 'Por favor inicie a sessão %1$s.';
+$txt['login_below_register'] = 'Por favor inicie a sessão ou <a href="%1$s">registe uma conta</a> com %2$s.';
+
+$txt['login_hash_error'] = 'Requisitos de segurança atualizados. Inicie de novo a sessão por favor.';
+
+$txt['ban_register_prohibited'] = 'Desculpe, não lhe é permitido registar uma conta neste fórum.';
+
+$txt['activate_account'] = 'Ativação de Conta';
+$txt['activate_success'] = 'A sua conta foi ativada com sucesso. Pode agora iniciar a sessão.';
+$txt['activate_not_completed1'] = 'Tem de validar o seu email para iniciar a sessão.';
+$txt['activate_not_completed2'] = 'Solicitar novo email de ativação';
+$txt['activate_after_registration'] = 'Obrigado por se registar. Receberá um email em breve com um link para ativar a sua conta. Se não recebeu nenhum email verifique a pasta de spam.';
+$txt['invalid_userid'] = 'Utilizador não existe';
+$txt['invalid_activation_code'] = 'Código de ativação inválido';
+$txt['invalid_activation_username'] = 'Nome de Utilizador ou email';
+$txt['invalid_activation_new'] = 'Se se registou com um email errado, introduza um novo e a sua palavra-passe aqui.';
+$txt['invalid_activation_new_email'] = 'novo endereço de email';
+$txt['invalid_activation_password'] = 'Palavra-apsse antiga';
+$txt['invalid_activation_resend'] = 'Reenviar código de ativação';
+$txt['invalid_activation_known'] = 'Se já sabe o seu código de ativação, introduza-o aqui.';
+$txt['invalid_activation_retry'] = 'Código de ativação';
+$txt['invalid_activation_submit'] = 'Ativar';
+
+$txt['coppa_no_concent'] = 'O administrador ainda não recebeu permissões parentais para a sua conta.';
+$txt['coppa_need_more_details'] = 'Necessita de mais detalhes?';
+
+$txt['awaiting_delete_account'] = 'A sua conta foi marcada para ser removida!<br>Se desejar recuperar a sua conta, por favor aceda a &quot;Reativar conta&quot;, e inicie a sessão de novo.';
+$txt['undelete_account'] = 'Reativar conta';
+
+// Use numeric entities in the below three strings.
+$txt['change_password'] = 'Detalhes da nova palavra-apsse';
+$txt['change_password_login'] = 'Dados de inicio de sessão em';
+$txt['change_password_new'] = 'foram alterados e a palavra-passe redefinida. Abaixo estão os novos dados de inicio de sessão.';
+
+$txt['in_maintain_mode'] = 'Este fórum encontra-se em Manutenção.';
+
+$txt['register_agree'] = 'Por favor leia e aceite os termos de utilização antes de se registar.';
+$txt['register_passwords_differ_js'] = 'As duas palavras-passes não coincidem!';
+
+$txt['approval_after_registration'] = 'Obrigado por se registar. O administrador tem de aprovar a sua conta antes de poder utiliza-la, receberá em breve um email com a decisão do administrador.';
+
+$txt['admin_settings_desc'] = 'Aqui pode alterar várias configurações relacionadas com o registo de contas.';
+
+$txt['setting_registration_method'] = 'Método de registo para novos utilizadores';
+$txt['setting_registration_disabled'] = 'Registos Desativados';
+$txt['setting_registration_standard'] = 'Registo Imediato';
+$txt['setting_registration_activate'] = 'Ativação por Email';
+$txt['setting_registration_approval'] = 'Ativação por Aprovação';
+$txt['setting_registration_both'] = 'Ativação por email seguido de Aprovação pelo Admin';
+$txt['setting_notify_new_registration'] = 'Notificar Administradores do registo de novas contas';
+$txt['setting_send_welcomeEmail'] = 'Enviar email de boas-vindas aos novos utilizadores';
+$txt['purge_unactivated_days'] = 'Remover contas não ativadas com quantos dias?';
+$txt['purge_unactivated_days_subtext'] = 'Se definido com 0, não serão removidas.';
+$txt['purge_unactivated_days_postinput'] = 'dias';
+
+$txt['setting_coppaAge'] = 'Idade mínima para aplicar restrições de registo';
+$txt['setting_coppaAge_desc'] = '(Definir 0 para desativar)';
+$txt['setting_coppaType'] = 'Ação a executar quando utilizador com idade inferior regista-se';
+$txt['setting_coppaType_reject'] = 'Rejeitar registo';
+$txt['setting_coppaType_approval'] = 'Requerer aprovação paternal';
+$txt['setting_coppaPost'] = 'Endereço para o qual as aprovações devem de ser enviadas';
+$txt['setting_coppaPost_desc'] = 'Apenas aplicada se configurada a restrição por idade';
+$txt['setting_coppaFax'] = 'Fax para onde os formulários de aprovação devem de ser enviados';
+$txt['setting_coppaPhone'] = 'Contato para o qual os país devem de contatar com pedidos de registo';
+
+$txt['admin_register'] = 'Registo de um novo utilizador';
+$txt['admin_register_desc'] = 'Apartir de aqui pode registar novos membros, e se desejado, enviar por email os dados.';
+$txt['admin_register_username'] = 'Novo nome de utilizador';
+$txt['admin_register_email'] = 'Endereço de Email';
+$txt['admin_register_password'] = 'Palavra-Passe';
+$txt['admin_register_username_desc'] = 'Nome de utilizador do novo membro';
+$txt['admin_register_email_desc'] = 'Endereço de Email do novo membro';
+$txt['admin_register_password_desc'] = 'Palavra-Passe do novo membro';
+$txt['admin_register_email_detail'] = 'Enviar nova palavra-passe ao utilizador';
+$txt['admin_register_email_detail_desc'] = 'Endereço de Email necessário mesmo que não selecionado';
+$txt['admin_register_email_activate'] = 'Utilizador necessário para ativar conta';
+$txt['admin_register_group'] = 'Grupo Primário';
+$txt['admin_register_group_desc'] = 'Grupo Primário para novos utilizadores';
+$txt['admin_register_group_none'] = '(sem grupo primário)';
+$txt['admin_register_done'] = 'Utilizador %1$s foi registado com sucesso!';
+
+$txt['coppa_title'] = 'Fórum com restrição de Idade';
+$txt['coppa_after_registration'] = 'Obrigado por se registar com o nome {forum_name_safe}.<br><br>Por ter idade inferior a {MINIMUM_AGE}, é legalmente requerido
+	obter permissões parentais antes de poder utilizar a sua conta. Para ativar a conta imprima o formulário abaixo:';
+$txt['coppa_form_link_popup'] = 'Carregar formulário numa nova janela';
+$txt['coppa_form_link_download'] = 'Descarregar formulário com ficheiro de texto';
+$txt['coppa_send_to_one_option'] = 'Depois então envie o formuário preenchido por:';
+$txt['coppa_send_to_two_options'] = 'Depois então envie o formuário preenchido por:';
+$txt['coppa_send_by_post'] = 'Envie para seguinte endereço:';
+$txt['coppa_send_by_fax'] = 'Fax para o seguinte número:';
+$txt['coppa_send_by_phone'] = 'Alternativamente, contate o administrador por telefone {PHONE_NUMBER}.';
+
+$txt['coppa_form_title'] = 'Formulário de ativação a {forum_name_safe}';
+$txt['coppa_form_address'] = 'Endereço';
+$txt['coppa_form_date'] = 'Data';
+$txt['coppa_form_body'] = 'Eu {PARENT_NAME},<br><br>Dou permissão a {CHILD_NAME} (child name) para se tornar um membro registado no fórum: {forum_name_safe}, com o nome de utilizador: {USER_NAME}.<br><br>Tomei conhecimento de que qualquer informação pessoal publicada por {USER_NAME} poderá ser visivel a outros utilizadores do fórum.<br><br>Assinado:<br>{PARENT_NAME} (Parent/Guardian).';
+
+$txt['visual_verification_sound_again'] = 'Tocar de novo';
+$txt['visual_verification_sound_direct'] = 'Com problemas a ouvir isto? Tente um link direto ao som.';
+
+$txt['registration_username_available'] = 'Nome de utilizador é inválido';
+$txt['registration_username_unavailable'] = 'Nome de utilizador não disponível';
+$txt['registration_username_check'] = 'Verificar se nome de utilizador se se encontra disponível';
+$txt['registration_password_short'] = 'Palavra-passe demasiado curta';
+$txt['registration_password_reserved'] = 'Palavra-passe contém o seu nome de utilizador/email';
+$txt['registration_password_numbercase'] = 'Palavra-passe deve de conter letras maiúsculas, minúsculas e números';
+$txt['registration_password_no_match'] = 'Palavras-passe não coincidem';
+$txt['registration_password_valid'] = 'Palavras-passe é válida';
+
+$txt['registration_errors_occurred'] = 'Os seguintes erros foram detetados no seu registo. Por favor corriga-os para continuar:';
+$txt['error_invalid_characters_username'] = 'Carácter inválido usado no nome de utilizador.';
